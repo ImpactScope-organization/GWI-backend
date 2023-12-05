f1de294982bf26c31daf5fc2398d9d61428ec1ad
mzmalik22 <zulqurnain.es7@gmail.com>
2023-12-05 17:09:42 +0500

    mongodb connection changes added

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then((data) => {
-      console.log(`Monogodb connected with server`);
-      console.log(`Monogodb connected with server : ${data.connection.host} `);
-    })
-    .catch((err) => console.log("Error connecting to server", err));
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log(`Database connected successfully : ${process.env.MONGO_URL}`);
+  } catch (error) {
+    console.log("Error connecting to Mongo");
+  }
 };
 
 // module.exports = connectDatabase
