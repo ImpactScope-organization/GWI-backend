f656d8b54eb825faae4728855fc65981bb0a30ea
mzmalik22 <zulqurnain.es7@gmail.com>
2023-12-05 16:59:44 +0500

    minor changes added

--- config/db.js
+++ config/db.js
@@ -10,31 +10,5 @@ const connectDatabase = () => {
     .catch((err) => console.log("Error connecting to server", err));
 };
 
-// Check the connection status
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-db.once("connected", () => {
-  console.log("Connected to MongoDB!");
-});
-
-db.on("disconnected", () => {
-  console.log("Disconnected from MongoDB");
-});
-
-// Function to gracefully close the connection
-const gracefulExit = () => {
-  mongoose.connection.close(() => {
-    console.log(
-      "Mongoose default connection disconnected through app termination"
-    );
-    process.exit(0);
-  });
-};
-
-// If Node.js process ends, close the Mongoose connection
-process.on("SIGINT", gracefulExit).on("SIGTERM", gracefulExit);
-
 // module.exports = connectDatabase
 module.exports = connectDatabase;
